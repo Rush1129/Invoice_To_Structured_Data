7e1ee863ef8b701f7f86df2fc7200b7b9ad00831
Rush1129 <rushikeshvekaria29@gmail.com>
2025-03-12 18:42:27 +0530

    Initial Project Upload

--- main.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load YOLOv8 model (pretrained)
-model = YOLO("yolov8n.pt")  # Use 'yolov8m.pt' or 'yolov8l.pt' for larger models
-
-# Train the model
-model.train(data="dataset/dataset.yaml", epochs=1, imgsz=640, batch=16)
